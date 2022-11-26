07be61b1e3a5e0f3a972becc33eddf967af2c305
kmjang <kyumin.jang@gmail.com>
2022-11-27 01:23:15 +0900

    apollo-odyssey 3단계 완료

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,23 +1,22 @@
 const resolvers = {
   Query: {
-    // get all tracks, will be used to populate the homepage grid of our web client
+    // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
-    tracksForHomeFetch: async () => {
-      const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
-      const res = await fetch(`${baseUrl}/tracks`);
-      return res.json();
+
+    // get a single track by ID, for the track page
+    track: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id);
     },
   },
   Track: {
-    // highlight-start
-    author: async ({ authorId }, _, { dataSources }) => {
-      const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
-      const res = await fetch(`${baseUrl}/author/${authorId}`);
-      return res.json();
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
 
-      // return dataSources.trackAPI.getAuthor(authorId);
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id);
     },
   },
 };
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,12 +1,11 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-  # write your schema definitions here
   type Query {
+    "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
-    tracksForHomeFetch: [Track!]!
-    modules: [Module!]!
-    track: Track
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track!
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -14,35 +13,37 @@ const typeDefs = gql`
     id: ID!
     "The track's title"
     title: String!
-    "The track's main author"
+    "The track's main Author"
     author: Author!
-    "The track's main illustration to display in track card or track page detail"
+    "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
-    "The track's complete description, can be in Markdown format"
+    "The track's complete description, can be in markdown format"
     description: String
     "The number of times a track has been viewed"
     numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
   }
 
-  "Author of a complete Track"
+  "Author of a complete Track or a Module"
   type Author {
     id: ID!
     "Author's first and last name"
     name: String!
-    "Author's profile picture url"
+    "Author's profile picture"
     photo: String
   }
 
   "A Module is a single unit of teaching. Multiple Modules compose a Track"
   type Module {
     id: ID!
-    "The Module's title"
+    "The module's title"
     title: String!
-    "The Module's length in minutes"
+    "The module's length in minutes"
     length: Int
   }
 `;
